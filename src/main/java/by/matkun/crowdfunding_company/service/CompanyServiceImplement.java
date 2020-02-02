00949a9a30b4f1d6126380a1b6724d24c29aa5e0
Viktor <33760375+matkun81@users.noreply.github.com>
2020-02-02 23:29:28 +0300

    save image in cloud

--- src/main/java/by/matkun/crowdfunding_company/service/CompanyServiceImplement.java
+++ src/main/java/by/matkun/crowdfunding_company/service/CompanyServiceImplement.java
@@ -2,6 +2,9 @@ package by.matkun.crowdfunding_company.service;
 
 import by.matkun.crowdfunding_company.dao.CompanyRepository;
 import by.matkun.crowdfunding_company.model.Company;
+import com.cloudinary.Cloudinary;
+import com.cloudinary.Transformation;
+import com.cloudinary.utils.ObjectUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
@@ -9,8 +12,7 @@ import org.springframework.web.multipart.MultipartFile;
 
 import java.io.File;
 import java.io.IOException;
-import java.util.List;
-import java.util.UUID;
+import java.util.*;
 
 
 @Service
@@ -43,16 +45,26 @@ public class CompanyServiceImplement implements CompanyService  {
 
     @Override
     public void uploadImage(Company company, MultipartFile file) throws IOException {
+        Map config = new HashMap();
+        config.put("cloud_name", "matkun");
+        config.put("api_key", "322761257467638");
+        config.put("api_secret", "0Z0KmB0457_7s_bZO-w1WR27iYs");
+        Cloudinary cloudinary = new Cloudinary(config);
 
-        if (file != null) {
+        if (file != null && !file.getOriginalFilename().isEmpty()) {
             File uploadDir = new File(uploadPath);
             if (!uploadDir.exists()) {
                 uploadDir.mkdir();
             }
-            String uuidFile = UUID.randomUUID().toString();
-            String resultFileName = uuidFile + "." + file.getOriginalFilename();
-            file.transferTo(new File(uploadPath + "/" + resultFileName));
-            company.setFileName(resultFileName);
+            file.transferTo(new File(uploadPath + "/" + file.getOriginalFilename()));
+           Map uploadResult = cloudinary.uploader().upload(uploadPath + "/" + file.getOriginalFilename(),ObjectUtils.emptyMap());
+            for (File myFile: Objects.requireNonNull(new File(uploadPath).listFiles())){
+                if (myFile.isFile()) {
+                    myFile.delete();
+                }
+            }
+           String url = String.valueOf(uploadResult.get("url"));
+            company.setFileName(url);
         }
     }
 }
